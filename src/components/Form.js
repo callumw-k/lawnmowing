0a0998d288536a2cc399e6216e05bd948ba79706
Callum Wellwood-Kane <cwellkane@gmail.com>
2020-12-22 14:35:24 +1100

    form working

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,48 +15,76 @@ const FormBox = styled.div`
   flex: 1;
   margin-bottom: 1rem;
   label {
-  margin-bottom: 0.5rem;
+    margin-bottom: 0.5rem;
   }
   input {
-  padding: 0.5rem 1rem;
-  border-radius: 10px;
-  border: none;
-  &:focus {
-  outline: none;
+    padding: 0.5rem 1rem;
+    border-radius: 10px;
+    border: none;
+    &:focus {
+      outline: none;
+    }
   }
-  }
-  
 `
 
 const NameWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-gap: 2%;
-  
 `
 
-export default function Form() {
-  return (
-    <FormOuter action="">
-      <NameWrapper>
-        <FormBox>
-          <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" />
-        </FormBox>
+export default class Form extends React.Component {
+  state = {
+    status: "",
+  }
+
+  submitForm =(ev) => {
+    ev.preventDefault()
+    const form = ev.target
+    const data = new FormData(form)
+    const xhr = new XMLHttpRequest()
+    xhr.open(form.method, form.action)
+    xhr.setRequestHeader("Accept", "application/json")
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return
+      if (xhr.status === 200) {
+        form.reset()
+        this.setState({ status: "SUCCESS" })
+      } else {
+        this.setState({ status: "ERROR" })
+      }
+    }
+    xhr.send(data)
+  }
+
+  render() {
+    const { status } = this.state
+    return (
+      <FormOuter
+        onSubmit={this.submitForm}
+        action="https://formspree.io/f/xknppgdo"
+        method="POST"
+
+      >
+        <NameWrapper>
+          <FormBox>
+            <label htmlFor="first-name">First Name</label>
+            <input name={'First Name'} type="text" id="first-name" />
+          </FormBox>
+
+          <FormBox>
+            <label htmlFor="last-name">Last Name</label>
+            <input name={'Last Name'} type="text" id="last-name" />
+          </FormBox>
+        </NameWrapper>
 
         <FormBox>
-          <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" />
+          <label htmlFor={"phone"}>Phone Number</label>
+          <input name={'Telephone Number'} type={"tel"} id={"phone"} />
         </FormBox>
-
-
-
-      </NameWrapper>
-
-      <FormBox >
-        <label htmlFor={'phone'} >Phone Number</label>
-        <input  type={'tel'} id={'phone'}/>
-      </FormBox>
-    </FormOuter>
-  )
+        {status === "SUCCESS" ? <p>Thanks!</p> : <button>Submit</button>}
+        {status === "ERROR" && <p>Ooops! There was an error.</p>}
+      </FormOuter>
+    )
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,24 @@ import React from "react"
 
 import Hero from "../components/index/Hero"
 import About from "../components/index/About"
+import Form from "../components/Form"
+import MyForm from "../components/DefaultForm"
+
+
+
 
 export default function Home() {
+
+
+
   return (
     <React.Fragment>
       <Hero />
       <About />
+      <Form/>
+      <MyForm/>
+
+
     </React.Fragment>
   )
 }
